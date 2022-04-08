26800c5a0edf11511e8d8541a306252e463db863
Zironics <rafikobouloudene@gmail.com>
2022-04-08 18:20:35 +0100

    commit add.

---
+++ javas.js
@@ -0,0 +1,15 @@
+let burger =document.querySelector(".burger");
+
+let mobilenav=document.querySelector(".mobile-nav");
+
+burger.addEventListener('click',function(e){
+    mobilenav.classList.toggle("display");
+   
+    burger.classList.toggle("toggle");
+});
+
+$(document).ready(function(){
+    $(".burger").click(function(){
+      $(".mobile-nav").slideToggle("slow");
+    });
+});
